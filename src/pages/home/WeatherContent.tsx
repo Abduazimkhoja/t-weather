2aa9ff1e1c799b0c82871339d1730ff7d2c1d607
Abduazimkhoja <abdulazimkhoja@gmail.com>
2025-09-17 15:40:45 +0500

    feat(home): enhance weather display
    - Added date-fns library
    - Improved date formatting
    - Updated styles for better UI

--- src/pages/home/WeatherContent.tsx
+++ src/pages/home/WeatherContent.tsx
@@ -1,44 +1,46 @@
+import { format } from "date-fns";
+import { enUS } from "date-fns/locale";
 import type { GetWeatherByCityResponse } from "@/store/services";
 
 export function WeatherContent({ weatherData }: Props) {
 	return (
 		<>
-			<div className="root-image-wrap">
-				<img alt="" className="" src="https://i.imgur.com/M8VyA2h.png" />
+			<div className="bg-image-wrap">
+				<img alt="airship" src="https://i.imgur.com/M8VyA2h.png" />
 			</div>
 
-			<div className="flex flex-row flex-x-center gap-5 info-container">
-				<h1 className="root-heading">
+			<div className="degree">
+				<h1 className="degree-title">
 					{Math.round(weatherData.main.temp)}&#176;
 				</h1>
 
-				<div className="">
-					<h2 className="city-name">{weatherData.name}</h2>
-					<time
-						className="text-sm"
-						dateTime={new Date(weatherData.dt * 1000).toISOString()}
-					>
-						{new Date(weatherData.dt * 1000).toLocaleString("en-GB", {
-							hour: "2-digit",
-							minute: "2-digit",
-							weekday: "long",
-							day: "numeric",
-							month: "short",
-							year: "2-digit",
-						})}
-					</time>
-				</div>
-				<div className="flex-center flex-col text-center">
-					<div className="image-wrap">
-						<img
-							width={50}
-							height={50}
-							alt={weatherData.weather[0].description}
-							className=""
-							src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
-						/>
+				<div className="degree">
+					<div>
+						<h2 className="city-name">{weatherData.name}</h2>
+
+						<time
+							className="text-sm"
+							dateTime={new Date(weatherData.dt * 1000).toISOString()}
+						>
+							{format(
+								new Date(weatherData.dt * 1000),
+								"HH:mm - EEEE, dd MMM ''yy",
+								{ locale: enUS },
+							)}
+						</time>
 					</div>
-					<small className="text-sm">{weatherData.weather[0].main}</small>
+
+					<div className="flex-center flex-col text-center">
+						<div className="weather-icon">
+							<img
+								width={50}
+								height={50}
+								alt={weatherData.weather[0].description}
+								src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+							/>
+						</div>
+						<small className="text-sm">{weatherData.weather[0].main}</small>
+					</div>
 				</div>
 			</div>
 		</>
